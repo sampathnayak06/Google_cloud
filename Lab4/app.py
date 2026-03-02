18f59919f9c1d10475f79abc52e4e5c064703156
Sampath Nayak <sampath.23cs133@sode-edu.in>
2026-03-02 12:19:26 +0530

    Initialize Flask app with a home route

--- Lab4/app.py
+++ Lab4/app.py
@@ -1,1 +1,7 @@
-
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def home():
+  return "Hello Docker from Killercoda!"
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=8080)
